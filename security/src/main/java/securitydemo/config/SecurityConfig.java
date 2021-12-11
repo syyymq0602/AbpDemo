4fbb98c91ee2fe15dc7b5f556a3e65159634582c
PPG <295217270@qq.com>
2021-12-11 22:21:24 +0800

    定义登录失败跳转页面以及自定义表单提交的标签名

--- security/src/main/java/securitydemo/config/SecurityConfig.java
+++ security/src/main/java/securitydemo/config/SecurityConfig.java
@@ -14,8 +14,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // 表单提交
         http.formLogin()
+                // 自定义表单提交的用户名
+                .usernameParameter("user")
+                // 自定义表单提交的密码名
+                .passwordParameter("pass")
+                // 发现是/login时认为时登陆，路径必须和表单一致
+                .loginProcessingUrl("/login")
                 // 自定义登陆页面
-                .loginPage("/login.html");
+                .loginPage("/login.html")
+                    // 登陆后跳转页面，必须为POST请求
+                    .successForwardUrl("/toMain")
+                        // 登录失败跳转请求 必须POST请求
+                        .failureForwardUrl("/toError");
+
+        // 授权认证
+        http.authorizeRequests()
+                // 放行login.html
+                .antMatchers("/login.html","/error.html").permitAll()
+                // 所有请求都必须被认证
+                .anyRequest().authenticated();
+
+        http.csrf().disable();
     }
 
     @Bean
--- security/src/main/java/securitydemo/controller/LoginController.java
+++ security/src/main/java/securitydemo/controller/LoginController.java
@@ -1,6 +1,7 @@
 package securitydemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,9 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String login(){
-        System.out.println("执行登录方法");
+//    @RequestMapping("/login")
+//    public String login(){
+//        System.out.println("执行登录方法");
+//        return "redirect:main.html";
+//    }
+
+    @PostMapping("/toMain")
+    public String toMain(){
         return "redirect:main.html";
+    }
+
+    @PostMapping("/toError")
+    public String toError(){
+        return "redirect:error.html";
     }
 }
